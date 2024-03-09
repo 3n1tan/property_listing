509cf060014039e6250a0283f51e19d64657554b
Enitan Odupitan <odupitanenitan@gmail.com>
2024-03-09 19:58:16 +0200

    NavBar Modification

--- components/NavBar/app.tsx
+++ components/NavBar/app.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -21,54 +21,71 @@ import {
   Avatar,
   Badge,
 } from "@nextui-org/react";
-import {Icon} from "@iconify/react";
+import { Icon } from "@iconify/react";
 
-import {AcmeIcon} from "./social";
+import { AcmeIcon } from "./social";
 
 import NotificationsCard from "./notifications-card";
+import { ThemeSwitcher } from "../DarkMode/switch_theme";
+import Image from "next/image";
 
 export default function Component() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleLinkClick = () => {
+      setIsMenuOpen(false);
+    };
   return (
     <Navbar
       classNames={{
         base: "lg:bg-transparent lg:backdrop-filter-none",
         item: "data-[active=true]:text-primary",
-        wrapper: "px-4 sm:px-6",
+        wrapper: "px-4 sm:px-6 max-w-[110rem] py-9",
       }}
       height="60px"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarBrand>
-        <NavbarMenuToggle className="mr-2 h-6 sm:hidden" />
-        <AcmeIcon />
-        <p className="font-bold text-inherit">ACME</p>
+      <NavbarBrand className="max-w-fit lg:mr-9">
+        <NavbarMenuToggle className="mr-2 h-6 lg:hidden" />
+        <div className="flex items-center lg:ml-0 ml-[3rem]">
+          <Link href="/">
+            <AcmeIcon />
+            <p className="font-bold text-inherit">3N1T | PROPERTIES </p>
+          </Link>
+        </div>
       </NavbarBrand>
       <NavbarContent
-        className="ml-4 hidden h-12 w-full max-w-fit gap-4 rounded-full bg-content2 px-4 dark:bg-content1 sm:flex"
+        className="ml-[20rem] hidden h-12 w-full max-w-fit gap-9 rounded-full bg-content2 px-4 dark:bg-content1 lg:flex"
         justify="start"
       >
         <NavbarItem>
-          <Link className="flex gap-2 text-inherit" href="#">
-            Dashboard
+          <Link className="flex gap-2 text-inherit" href="/">
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link aria-current="page" className="flex gap-2 text-inherit" href="#">
-            Deployments
+          <Link
+            aria-current="page"
+            className="flex gap-2 text-inherit"
+            href="/listing"
+          >
+            Listing
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="flex gap-2 text-inherit" href="#">
-            Analytics
+          <Link className="flex gap-2 text-inherit" href="/listing/create">
+            Add Listing
           </Link>
         </NavbarItem>
+      </NavbarContent>
+      <NavbarContent
+        className="hidden lg:flex h-12 max-w-fit items-center gap-0 rounded-full p-0 lg:bg-content2 lg:px-5 lg:dark:bg-content1"
+        justify="center"
+      >
         <NavbarItem>
           <Link className="flex gap-2 text-inherit" href="#">
-            Team
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link className="flex gap-2 text-inherit" href="#">
-            Settings
+            Login or Register
           </Link>
         </NavbarItem>
       </NavbarContent>
@@ -76,19 +93,24 @@ export default function Component() {
         className="ml-auto flex h-12 max-w-fit items-center gap-0 rounded-full p-0 lg:bg-content2 lg:px-1 lg:dark:bg-content1"
         justify="end"
       >
-        <NavbarItem className="hidden sm:flex">
+        {/* <NavbarItem className="hidden sm:flex">
           <Button isIconOnly radius="full" variant="light">
             <Icon className="text-default-500" icon="solar:magnifer-linear" width={22} />
           </Button>
-        </NavbarItem>
+        </NavbarItem> */}
         <NavbarItem className="hidden sm:flex">
-          <Button isIconOnly radius="full" variant="light">
+          <ThemeSwitcher />
+          {/* <Button isIconOnly radius="full" variant="light">
             <Icon className="text-default-500" icon="solar:sun-linear" width={24} />
-          </Button>
+          </Button> */}
         </NavbarItem>
         <NavbarItem className="hidden sm:flex">
           <Button isIconOnly radius="full" variant="light">
-            <Icon className="text-default-500" icon="solar:settings-linear" width={24} />
+            <Icon
+              className="text-default-500"
+              icon="solar:settings-linear"
+              width={24}
+            />
           </Button>
         </NavbarItem>
         <NavbarItem className="flex">
@@ -102,7 +124,11 @@ export default function Component() {
                 variant="light"
               >
                 <Badge color="danger" content="5" showOutline={false} size="md">
-                  <Icon className="text-default-500" icon="solar:bell-linear" width={22} />
+                  <Icon
+                    className="text-default-500"
+                    icon="solar:bell-linear"
+                    width={22}
+                  />
                 </Badge>
               </Button>
             </PopoverTrigger>
@@ -123,7 +149,13 @@ export default function Component() {
                   shape="circle"
                   size="sm"
                 >
-                  <Avatar size="sm" src="https://i.pravatar.cc/150?u=a04258114e29526708c" />
+                  <Image
+                    src="https://i.pravatar.cc/150?u=a04258114e29526708c"
+                    width={30}
+                    height={30}
+                    alt=""
+                    className="rounded-full"
+                  />
                 </Badge>
               </button>
             </DropdownTrigger>
@@ -137,7 +169,9 @@ export default function Component() {
               <DropdownItem key="analytics">Analytics</DropdownItem>
               <DropdownItem key="system">System</DropdownItem>
               <DropdownItem key="configurations">Configurations</DropdownItem>
-              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+              <DropdownItem key="help_and_feedback">
+                Help & Feedback
+              </DropdownItem>
               <DropdownItem key="logout" color="danger">
                 Log Out
               </DropdownItem>
@@ -147,31 +181,29 @@ export default function Component() {
       </NavbarContent>
 
       {/* Mobile Menu */}
-      <NavbarMenu>
+      <NavbarMenu className="flex items-center gap-[3rem] pt-[3rem]">
         <NavbarMenuItem>
-          <Link className="w-full" color="foreground" href="#">
-            Dashboard
-          </Link>
-        </NavbarMenuItem>
-        <NavbarMenuItem isActive>
-          <Link aria-current="page" className="w-full" color="primary" href="#">
-            Deployments
+          <Link className="w-full" color="foreground" href="/" onClick={handleLinkClick}>
+            Home
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link className="w-full" color="foreground" href="#">
-            Analytics
+          <Link className="w-full" color="foreground" href="/listing" onClick={handleLinkClick}>
+            Listing
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link className="w-full" color="foreground" href="#">
-            Team
+          <Link className="w-full" color="foreground" href="/listing/create" onClick={handleLinkClick}>
+            Add Listing
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link className="w-full" color="foreground" href="#">
-            Settings
+          <Link className="flex gap-2 text-inherit" href="#" onClick={handleLinkClick}>
+            Login or Register
           </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <ThemeSwitcher />
         </NavbarMenuItem>
       </NavbarMenu>
     </Navbar>
