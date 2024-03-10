e32803d991faf000ebed75038fca5908ee1c2b03
Enitan Odupitan <odupitanenitan@gmail.com>
2024-03-10 19:49:02 +0200

    hero_banner desktop implemented

--- components/UI/HeroBanner/hero.tsx
+++ components/UI/HeroBanner/hero.tsx
@@ -1,15 +1,36 @@
+"use client";
 import React from "react";
-import { Input } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import { animals } from "./data";
 
 const Hero = () => {
   return (
     <div className="max-w-[90rem] mx-auto px-9 pt-[7rem]">
-      <div className="">
-        <h1 className="text-[3rem] font-extrabold text-center tracking-widest pb-3">Discover The Ideal Rental</h1>
-        <p className="text-center tracking-widest text-[16px] pb-9">Explore the ideal property tailored to your needs</p>
-        <div className="flex flex-wrap md:flex-nowrap gap-4">
-          <Input type="email" label="Email" />
-          <Input type="email" label="Email" placeholder="Enter your email" />
+      <div className="max-w-[50rem] mx-auto">
+        <h1 className="text-[3rem] font-extrabold text-center tracking-widest pb-3">
+          Discover The Ideal Rentals
+        </h1>
+        <p className="text-center tracking-widest text-[16px] pb-9">
+          Explore the ideal property tailored to your needs.
+        </p>
+        <div className="flex flex-wrap md:flex-nowrap gap-6">
+          <Input
+            type="text"
+            label=""
+            placeholder="Enter Location (City, State, Zip, etc)"
+            className="max-w-["
+            size="lg"
+          />
+          <Select label="All" className="max-w-xs" size="sm">
+            {animals.map((animal) => (
+              <SelectItem key={animal.value} value={animal.value}>
+                {animal.label}
+              </SelectItem>
+            ))}
+          </Select>
+          <Button color="primary" className="h-12 bg-blue-500">
+            Search
+          </Button>
         </div>
       </div>
     </div>
