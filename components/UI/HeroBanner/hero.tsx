7aa2a9d4b0bf145d642fff162f49cd7e52c8924d
Enitan Odupitan <odupitanenitan@gmail.com>
2024-03-10 20:09:21 +0200

    hero_banner mobile view implemented

--- components/UI/HeroBanner/hero.tsx
+++ components/UI/HeroBanner/hero.tsx
@@ -5,12 +5,12 @@ import { animals } from "./data";
 
 const Hero = () => {
   return (
-    <div className="max-w-[90rem] mx-auto px-9 pt-[7rem]">
-      <div className="max-w-[50rem] mx-auto">
-        <h1 className="text-[3rem] font-extrabold text-center tracking-widest pb-3">
+    <div className="max-w-[90rem] mx-auto px-9 lg:pt-[7rem] pt-[1rem]">
+      <div className="lg:max-w-[50rem] mx-auto max-w-fit">
+        <h1 className="lg:text-[3rem] text-3xl font-extrabold text-center tracking-widest lg:pb-3 pb-5">
           Discover The Ideal Rentals
         </h1>
-        <p className="text-center tracking-widest text-[16px] pb-9">
+        <p className="text-center tracking-widest lg:text-[16px] text-sm pb-9">
           Explore the ideal property tailored to your needs.
         </p>
         <div className="flex flex-wrap md:flex-nowrap gap-6">
@@ -18,17 +18,17 @@ const Hero = () => {
             type="text"
             label=""
             placeholder="Enter Location (City, State, Zip, etc)"
-            className="max-w-["
+            className="lg:max-w-fit"
             size="lg"
           />
-          <Select label="All" className="max-w-xs" size="sm">
+          <Select label="All" className="max-w-sm" size="sm">
             {animals.map((animal) => (
               <SelectItem key={animal.value} value={animal.value}>
                 {animal.label}
               </SelectItem>
             ))}
           </Select>
-          <Button color="primary" className="h-12 bg-blue-500">
+          <Button color="primary" className="h-12 bg-blue-500 w-full lg:w-fit">
             Search
           </Button>
         </div>
