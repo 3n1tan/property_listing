ba4134eb11f303fbdd92abf443b0b7634d99373f
Enitan Odupitan <odupitanenitan@gmail.com>
2024-03-15 18:42:30 +0200

    singleList page details refactoring

--- app/listing/[id]/page.tsx
+++ app/listing/[id]/page.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useState, useEffect } from "react";
 import SingleListCard from "@/components/UI/ListCard/singleListCard";
-import ListCard from "@/components/UI/ListCard/listCard";
 import {
   Card,
   CardHeader,
@@ -17,7 +16,7 @@ import Link from "next/link";
 import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
 import BathtubOutlinedIcon from "@mui/icons-material/BathtubOutlined";
 import SpaceDashboardOutlinedIcon from "@mui/icons-material/SpaceDashboardOutlined";
-
+import CheckIcon from "@mui/icons-material/Check";
 
 interface SingleListProps {
   params: {
@@ -56,13 +55,16 @@ const SingleListPage: FC<SingleListProps> = async ({ params }) => {
           <h1 className="text-default-400">{singleList.type}</h1>
         </CardHeader>
         <CardBody className="gap-5 dark:text-black">
-          <p className="lg:text-4xl text-3xl font-semibold tracking-wide">{singleList.name}</p>
+          <p className="lg:text-4xl text-3xl font-semibold tracking-wide">
+            {singleList.name}
+          </p>
           <p className="flex justify-start gap-4 text-sm items-center">
-            <LocationOnOutlinedIcon className="dark:text-yellow-400 text-red-400" /> {singleList.location.street}
-            , {singleList.location.city} {singleList.location.state}
+            <LocationOnOutlinedIcon className="dark:text-yellow-400 text-red-400" />{" "}
+            {singleList.location.street}, {singleList.location.city}{" "}
+            {singleList.location.state}
           </p>
         </CardBody>
-        <h2 className="bg-green-500 py-3 text-lg  pl-8 mx-3 mt-4 rounded-t-lg lg:mb-2 mb-8">
+        <h2 className="bg-green-500 py-3 pl-8 mx-3 mt-4 rounded-t-lg lg:mb-2 mb-8 font-semibold text-xl">
           Rates & Options
         </h2>
         <div className="lg:flex lg:flex-row lg:justify-around lg:mt-9 lg:mb-9 mb-6 grid gap-y-9">
@@ -70,7 +72,9 @@ const SingleListPage: FC<SingleListProps> = async ({ params }) => {
             <div>
               {singleList.rates.nightly ? (
                 <div className="flex items-center gap-3">
-                  <p className="text-default-500 text-sm lg:text-[1rem]">Nightly</p>
+                  <p className="text-default-500 text-sm lg:text-[1rem]">
+                    Nightly
+                  </p>
                   <p className="lg:text-2xl text-lg text-blue-400 font-bold">
                     {singleList.rates.nightly.toLocaleString()}€
                   </p>
@@ -84,7 +88,9 @@ const SingleListPage: FC<SingleListProps> = async ({ params }) => {
             <div className="">
               {singleList.rates.weekly && (
                 <div className="flex items-center gap-3">
-                  <p className="text-default-500 text-sm lg:text-[1rem]">Weekly</p>
+                  <p className="text-default-500 text-sm lg:text-[1rem]">
+                    Weekly
+                  </p>
                   <p className="lg:text-2xl text-xl text-blue-400 font-bold">
                     {singleList.rates.weekly.toLocaleString()} €/wk
                   </p>
@@ -96,7 +102,9 @@ const SingleListPage: FC<SingleListProps> = async ({ params }) => {
             <div className="">
               {singleList.rates.monthly && (
                 <div className="flex items-center gap-3">
-                  <p className="text-default-500 text-sm lg:text-[1rem]">Monthly</p>
+                  <p className="text-default-500 text-sm lg:text-[1rem]">
+                    Monthly
+                  </p>
                   <p className="lg:text-2xl text-xl text-blue-400 font-bold">
                     {singleList.rates.monthly.toLocaleString()} €/mo
                   </p>
@@ -111,50 +119,51 @@ const SingleListPage: FC<SingleListProps> = async ({ params }) => {
       </div>
       <Card className="bg-[#FBFFF4] dark:text-black">
         <CardHeader>
-          <h2 className="font-semibold">Description & Details</h2>
+          <h2 className="font-semibold text-xl">Description & Details</h2>
         </CardHeader>
         <CardBody>
-        <div className="flex justify-center mt-4  gap-5 text-blue-500">
-          <p>
-            <BedOutlinedIcon className="mr-2" />
-            <span className="font-medium text-md">{singleList.beds} Beds</span>
-          </p>
-          <p>
-            <BathtubOutlinedIcon className=" mr-2" />
-            <span className="font-medium text-md">{singleList.baths} Bath</span>
-          </p>
-          <p>
-            <SpaceDashboardOutlinedIcon className=" mr-2" />
-            <span className="font-medium text-md">
-              {singleList.square_feet} m&sup2;
-            </span>
-          </p>
-        </div>
+          <div className="flex justify-center mt-4  gap-5 text-blue-500">
+            <p>
+              <BedOutlinedIcon className="mr-2" />
+              <span className="font-medium text-md">
+                {singleList.beds} Beds
+              </span>
+            </p>
+            <p>
+              <BathtubOutlinedIcon className=" mr-2" />
+              <span className="font-medium text-md">
+                {singleList.baths} Bath
+              </span>
+            </p>
+            <p>
+              <SpaceDashboardOutlinedIcon className=" mr-2" />
+              <span className="font-medium text-md">
+                {singleList.square_feet} m&sup2;
+              </span>
+            </p>
+          </div>
         </CardBody>
         <CardFooter className="flex justify-center tracking-wide lg:mt-[1rem] mb-8">
-          <p className="text-sm text-center">
-            {singleList.description}
-          </p>
+          <p className="text-sm text-center">{singleList.description}</p>
         </CardFooter>
       </Card>
       <div className="lg:mt-8 lg:mb-8 mt-4 mb-4">
         <Divider />
       </div>
-      <Card className="bg-[#FBFFF4] dark:text-black">
+      <Card className="bg-[#FBFFF4] dark:text-black pb-9">
         <CardHeader>
-          <h2 className="font-semibold">Amenities</h2>
+          <h2 className="font-semibold text-xl">Amenities</h2>
         </CardHeader>
         <CardBody>
-          <ul>
-            {Array.isArray(singleList.amenities) }
-
+          <ul className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 list-none space-y-2">
+            {Array.isArray(singleList.amenities) &&
+              singleList.amenities.map((amenity: string[], index: number) => (
+                <li key={index} className="inline-block font-semibold">
+                  <CheckIcon className="text-green-700" /> {amenity}
+                </li>
+              ))}
           </ul>
         </CardBody>
-        <CardFooter className="flex justify-center tracking-wide lg:mt-[1rem] mb-8">
-          <p className="text-sm text-center">
-            {singleList.description}
-          </p>
-        </CardFooter>
       </Card>
     </div>
   );
